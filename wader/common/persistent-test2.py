a6f55a537d34a3e613ca18e0750de7204e26c3b9
David Francos Cuartero <dfrancos@warp.es>
2009-10-16 17:50:15 +0000

    Cleared some.

--- wader/common/persistent-test2.py
+++ wader/common/persistent-test2.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2006-2008  Vodafone España, S.A.
 # Copyright (C) 2008-2009  Warp Networks, S.L.
-# Author:  Nicholas Herriot & XanHQ
+# Author:  Pablo Martí
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -18,33 +18,27 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 """Data persistence for Wader"""
 
-#from sqlite3 import dbapi2 as sqlite
-
 from sqlite3 import dbapi2 as sqlite
-
 import wader.common.consts as consts
 
-db_connection = sqlite.connect('sample.db')
+conn = sqlite.connect('sample.db')
+curs = conn.cursor()
 
-db_curs = db_connection.cursor()
+curs.execute("""CREATE TABLE network (
+    networkID             INTEGER PRIMARY KEY AUTOINCREMENT,
+    networkName           TEXT,
+    country               TEXT) """)
 
-db_curs.execute("CREATE TABLE people (id INTEGER PRIMARY KEY, first_name VARCHAR(20),last_name VARCHAR(30), date_of_birth DATE)")
-
-db_curs.execute("""CREATE TABLE network (
-    networkID             INTEGER PRIMARY KEY AUTOINCREMENT, 
-    networkName        TEXT, 
-    country                 TEXT) """)
-
-db_curs.execute("""CREATE TABLE messages (
-    id                          INTEGER PRIMARY KEY AUTOINCREMENT, 
-    smsc                     TEXT, 
-    mmsc                    TEXT,
+curs.execute("""CREATE TABLE messages (
+    id                         INTEGER PRIMARY KEY AUTOINCREMENT,
+    smsc                       TEXT,
+    mmsc                       TEXT,
     type                       TEXT,
-    networkID              INTEGER NOT NULL,
+    networkID                  INTEGER NOT NULL,
     FOREIGN KEY(networkID) REFERENCES network(nedworkID))""")
-  
+#thread_id integer not null constraint fk_thread_id references thread(id) on delete cascade);
 
-db_curs.execute("""CREATE TABLE accessPointName (
+curs.execute("""CREATE TABLE accessPointName (
     id                          INTEGER PRIMARY KEY AUTOINCREMENT,
     apn                        TEXT,
     userName               TEXT,
@@ -53,28 +47,15 @@ db_curs.execute("""CREATE TABLE accessPointName (
     dns2                       TEXT,
     type                        TEXT,
     networkID               INTEGER NOT NULL CONSTRAINT fk_networkID REFERENCES network(nedworkID) ON DELETE CASCADE) """)
-    
 
-db_curs.execute("INSERT INTO people (first_name, last_name, date_of_birth) VALUES ('David', 'Bailey', '1938-1-2')")
+curs.execute("INSERT INTO people (first_name, last_name, date_of_birth) VALUES ('David', 'Bailey', '1938-1-2')")
+curs.execute("INSERT INTO network (networkID, networkName, country) VALUES ('23415', 'Vodafone UK', 'United Kingdom')")
+curs.execute("INSERT INTO messages (smsc , mmsc, type, networkID ) VALUES ('+44778501600523415', 'www.vodafone.com', 'contract', '23415')")
+curs.execute("INSERT INTO messages (smsc , mmsc, type, networkID ) VALUES ('+44778501600523415', 'www.vodafone.com', 'prepaid', '23415')")
+curs.execute("INSERT INTO accessPointName (apn , userName, passWord, dns1, dns2, type, networkID  ) VALUES ('internet', 'web', 'web', 'none', 'none', 'contract', '23415')")
+curs.execute("INSERT INTO accessPointName (apn , userName, passWord, dns1, dns2, type, networkID  ) VALUES ('pp.internet', 'web', 'web', 'none', 'none', 'prepaid', '23415')")
 
-db_curs.execute("INSERT INTO network (networkID, networkName, country) VALUES ('23415', 'Vodafone UK', 'United Kingdom')")
+conn.commit()
 
-db_curs.execute("INSERT INTO messages (smsc , mmsc, type, networkID ) VALUES ('+44778501600523415', 'www.vodafone.com', 'contract', '23415')")
-
-db_curs.execute("INSERT INTO messages (smsc , mmsc, type, networkID ) VALUES ('+44778501600523415', 'www.vodafone.com', 'prepaid', '23415')")
-
-db_curs.execute("INSERT INTO accessPointName (apn , userName, passWord, dns1, dns2, type, networkID  ) VALUES ('internet', 'web', 'web', 'none', 'none', 'contract', '23415')")
-
-db_curs.execute("INSERT INTO accessPointName (apn , userName, passWord, dns1, dns2, type, networkID  ) VALUES ('pp.internet', 'web', 'web', 'none', 'none', 'prepaid', '23415')")
-
-
-db_connection.commit()
-
-db_curs.execute("SELECT * FROM PEOPLE")
-db_curs.fetchall()
-
-
-
-
-
-
+curs.execute("SELECT * FROM PEOPLE")
+curs.fetchall()
