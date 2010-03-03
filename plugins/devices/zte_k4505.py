677520313df0630031d9a6143b105206fa5821ee
Pablo Martí Gamboa <pmarti@warp.es>
2010-03-03 14:52:30 +0000

    remove unneded code

--- plugins/devices/zte_k4505.py
+++ plugins/devices/zte_k4505.py
@@ -16,7 +16,6 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-from twisted.python import log
 from wader.common.hardware.zte import ZTEWCDMADevicePlugin
 
 
