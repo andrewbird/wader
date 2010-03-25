20127ab98704845076e80c62c4ee50d60744d59a
Pablo Martí Gamboa <pmarti@warp.es>
2010-03-25 15:09:48 +0000

    fix undefined name

--- wader/common/oses/unix.py
+++ wader/common/oses/unix.py
@@ -18,6 +18,8 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 """OSPlugin for Unix-based OSes"""
 
+from twisted.python import log
+
 from wader.common.plugin import OSPlugin
 from wader.common.resolvconf import NamedManager
 from wader.common.utils import get_file_data
