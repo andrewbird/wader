9ba06e75fba555511f89fd3f9e575e12d06bfe0a
Andrew Bird <ajb@spheresystems.co.uk>
2010-05-31 19:13:30 +0000

    Huawei K4505 - Authenticate after disable fix

--- plugins/devices/huawei_k4505.py
+++ plugins/devices/huawei_k4505.py
@@ -16,17 +16,52 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
+from twisted.internet import reactor
+from twisted.internet.task import deferLater
+
 from wader.common import consts
 from wader.common.hardware.base import build_band_dict
 from wader.common.hardware.huawei import (HuaweiWCDMADevicePlugin,
                                           HuaweiWCDMACustomizer,
+                                          HuaweiWCDMAWrapper,
                                           HUAWEI_BAND_DICT)
+
+
+class HuaweiK4505Wrapper(HuaweiWCDMAWrapper):
+    """
+    :class:`~wader.common.hardware.huawei.HuaweiWCDMAWrapper` for the K4505
+    """
+
+    def check_pin(self):
+        """
+        Returns the SIM's auth state
+
+        :raise SimPinRequired: Raised if SIM PIN is required
+        :raise SimPukRequired: Raised if SIM PUK is required
+        :raise SimPuk2Required: Raised if SIM PUK2 is required
+        """
+        # XXX: this device needs to be enabled before pin can be checked
+
+        d = self.get_radio_status()
+
+        def get_radio_status_cb(status):
+            if status != 1:
+                self.send_at('AT+CFUN=1')
+
+                # delay here 2 secs, or we perhaps we should wait for ^SRVST:1
+                return deferLater(reactor, 2, lambda: None)
+
+        d.addCallback(get_radio_status_cb)
+        d.addCallback(lambda x: super(HuaweiK4505Wrapper, self).check_pin())
+
+        return d
 
 
 class HuaweiK4505Customizer(HuaweiWCDMACustomizer):
     """
     :class:`~wader.common.hardware.huawei.HuaweiWCDMACustomizer` for the K4505
     """
+    wrapper_klass = HuaweiK4505Wrapper
 
     # GSM/GPRS/EDGE 850/900/1800/1900 MHz
     # HSDPA/UMTS 2100/900 MHz
@@ -52,6 +87,9 @@ class HuaweiK4505(HuaweiWCDMADevicePlugin):
     version = "0.1"
     author = u"Andrew Bird"
     custom = HuaweiK4505Customizer()
+
+    # After disable / enable we must re-authenticate the SIM
+    auth_persists_over_disable = False
 
     __remote_name__ = "K4505"
 
