8cf21b23b8a5ea1a0ddff49c8dae95ea5682a8d1
Andrew Bird <ajb@spheresystems.co.uk>
2010-07-15 20:54:02 +0000

    Huawei B970 - fix UTF-8 contacts

--- plugins/devices/huawei_b970.py
+++ plugins/devices/huawei_b970.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2009  Vodafone España, S.A.
+# Copyright (C) 2009-2010  Vodafone España, S.A.
 # Author:  Andrew Bird
 #
 # This program is free software; you can redistribute it and/or modify
@@ -16,8 +16,43 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-from wader.common.hardware.huawei import (HuaweiWCDMACustomizer,
-                                          HuaweiWCDMADevicePlugin)
+from wader.common.command import ATCmd
+from wader.common.encoding import pack_ucs2_bytes
+from wader.common.hardware.huawei import (HuaweiWCDMADevicePlugin,
+                                          HuaweiWCDMACustomizer,
+                                          HuaweiWCDMAWrapper)
+
+
+class HuaweiB970Wrapper(HuaweiWCDMAWrapper):
+    """
+    :class:`~wader.common.hardware.huawei.HuaweiWCDMAWrapper` for the B970
+    """
+
+    def _add_contact(self, name, number, index):
+        """
+        Adds a contact to the SIM card
+        """
+        raw = 0
+        try:     # are all ascii chars
+            name.encode('ascii')
+        except:  # write in TS31.101 type 80 raw format
+            # B970 doesn't need the "FF" suffix
+            # AT^CPBW=1,"28780808",129,"80534E4E3A",1
+            name = '80%s' % pack_ucs2_bytes(name)
+            raw = 1
+
+        category = 145 if number.startswith('+') else 129
+        args = (index, number, category, name, raw)
+        cmd = ATCmd('AT^CPBW=%d,"%s",%d,"%s",%d' % args, name='add_contact')
+
+        return self.queue_at_cmd(cmd)
+
+
+class HuaweiB970Customizer(HuaweiWCDMACustomizer):
+    """
+    :class:`~wader.common.hardware.huawei.HuaweiWCDMACustomizer` for the B970
+    """
+    wrapper_klass = HuaweiB970Wrapper
 
 
 class HuaweiB970(HuaweiWCDMADevicePlugin):
@@ -25,7 +60,7 @@ class HuaweiB970(HuaweiWCDMADevicePlugin):
     name = "Huawei B970"
     version = "0.1"
     author = u"Andrew Bird"
-    custom = HuaweiWCDMACustomizer
+    custom = HuaweiB970Customizer()
 
     __remote_name__ = "B970"
 
