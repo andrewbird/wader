857dc43ea9b43150af0859c0a20910ac1488ae4c
Andrew Bird <ajb@spheresystems.co.uk>
2010-07-15 20:53:58 +0000

    Huawei K2540 - fix UTF-8 contacts

--- plugins/devices/huawei_k2540.py
+++ plugins/devices/huawei_k2540.py
@@ -17,16 +17,45 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 from wader.common import consts
+from wader.common.command import ATCmd
+from wader.common.encoding import pack_ucs2_bytes
 from wader.common.hardware.base import build_band_dict
 from wader.common.hardware.huawei import (HuaweiWCDMADevicePlugin,
                                           HuaweiWCDMACustomizer,
+                                          HuaweiWCDMAWrapper,
                                           HUAWEI_BAND_DICT)
+
+
+class HuaweiK2540Wrapper(HuaweiWCDMAWrapper):
+    """
+    :class:`~wader.common.hardware.huawei.HuaweiWCDMAWrapper` for the K2540
+    """
+
+    def _add_contact(self, name, number, index):
+        """
+        Adds a contact to the SIM card
+        """
+        raw = 0
+        try:     # are all ascii chars
+            name.encode('ascii')
+        except:  # write in TS31.101 type 80 raw format
+            # K2540 doesn't need the "FF" suffix
+            # AT^CPBW=1,"28780808",129,"80534E4E3A",1
+            name = '80%s' % pack_ucs2_bytes(name)
+            raw = 1
+
+        category = 145 if number.startswith('+') else 129
+        args = (index, number, category, name, raw)
+        cmd = ATCmd('AT^CPBW=%d,"%s",%d,"%s",%d' % args, name='add_contact')
+
+        return self.queue_at_cmd(cmd)
 
 
 class HuaweiK2540Customizer(HuaweiWCDMACustomizer):
     """
     :class:`~wader.common.hardware.huawei.HuaweiWCDMACustomizer` for the K2540
     """
+    wrapper_klass = HuaweiK2540Wrapper
 
     # GSM/GPRS/EDGE 850/900/1800/1900 MHz
     band_dict = build_band_dict(
