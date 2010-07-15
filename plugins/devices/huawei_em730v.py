97090553406c9a981631581d238efee6db852f9a
Andrew Bird <ajb@spheresystems.co.uk>
2010-07-15 20:53:31 +0000

    Huawei EM730V - define possible set of bands

--- plugins/devices/huawei_em730v.py
+++ plugins/devices/huawei_em730v.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# Copyright (C) 2008-2010  Vodafone España, S.A.
 # Copyright (C) 2008-2009  Warp Networks, S.L.
 # Author:  Pablo Martí
 #
@@ -16,8 +17,32 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
+from wader.common import consts
+from wader.common.hardware.base import build_band_dict
 from wader.common.hardware.huawei import (HuaweiWCDMADevicePlugin,
-                                          HuaweiEMXXCustomizer)
+                                          HuaweiEMXXCustomizer,
+                                          HUAWEI_BAND_DICT)
+
+
+class HuaweiEM730VCustomizer(HuaweiEMXXCustomizer):
+    """
+    :class:`~wader.common.hardware.huawei.HuaweiWCDMACustomizer` for the E220
+    """
+
+    # GSM/GPRS/EDGE 850/900/1800/1900 MHz
+    # HSDPA/UMTS 900/1900/2100 MHz
+    band_dict = build_band_dict(
+                  HUAWEI_BAND_DICT,
+                  [consts.MM_NETWORK_BAND_ANY,
+
+                   consts.MM_NETWORK_BAND_G850,#  850
+                   consts.MM_NETWORK_BAND_EGSM,#  900
+                   consts.MM_NETWORK_BAND_DCS, # 1800
+                   consts.MM_NETWORK_BAND_PCS, # 1900
+
+#                   consts.MM_NETWORK_BAND_U900,
+                   consts.MM_NETWORK_BAND_U1900,
+                   consts.MM_NETWORK_BAND_U2100])
 
 
 class HuaweiEM730V(HuaweiWCDMADevicePlugin):
@@ -25,7 +50,7 @@ class HuaweiEM730V(HuaweiWCDMADevicePlugin):
     name = "Huawei EM730V"
     version = "0.1"
     author = u"Pablo Martí"
-    custom = HuaweiEMXXCustomizer()
+    custom = HuaweiEM730VCustomizer()
 
     __remote_name__ = "EM730V"
 
