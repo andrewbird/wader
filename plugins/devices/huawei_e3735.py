e6adb8fcd7078fe8b64fc6951286709a8d2fdd20
Andrew Bird <ajb@spheresystems.co.uk>
2010-07-15 20:54:29 +0000

    Huawei E3735 - enable fixes
    Note this device seems to reboot (disappears off bus) after entering
    PIN after device has been disabled/re-enabled, but it doesn't look like
    an application level problem.

--- plugins/devices/huawei_e3735.py
+++ plugins/devices/huawei_e3735.py
@@ -16,18 +16,56 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
+from twisted.internet import reactor
+from twisted.internet.task import deferLater
+
 from wader.common import consts
 from wader.common.hardware.base import build_band_dict
 from wader.common.hardware.huawei import (HuaweiWCDMADevicePlugin,
                                           HuaweiWCDMACustomizer,
+                                          HuaweiWCDMAWrapper,
                                           HUAWEI_BAND_DICT)
 
+
+class HuaweiK3735Wrapper(HuaweiWCDMAWrapper):
+    """
+    :class:`~wader.common.hardware.huawei.HuaweiWCDMAWrapper` for the K3735
+    """
+
+    def check_pin(self):
+        """
+        Returns the SIM's auth state
+
+        :raise SimPinRequired: Raised if SIM PIN is required
+        :raise SimPukRequired: Raised if SIM PUK is required
+        :raise SimPuk2Required: Raised if SIM PUK2 is required
+        """
+        # XXX: this device needs to be enabled before pin can be checked
+
+        d = self.get_radio_status()
+
+        def get_radio_status_cb(status):
+            if status != 1:
+                d = self.send_at('AT+CFUN=1')
+
+                def cb(arg):
+                    # delay here 2 secs
+                    return deferLater(reactor, 2, lambda: arg)
+
+                d.addCallback(cb)
+                return d
+
+        d.addCallback(get_radio_status_cb)
+        d.addCallback(lambda x: super(HuaweiK3735Wrapper, self).check_pin())
+
+        return d
 
 
 class HuaweiE3735Customizer(HuaweiWCDMACustomizer):
     """
     :class:`~wader.common.hardware.huawei.HuaweiWCDMACustomizer` for the E3735
     """
+    wrapper_klass = HuaweiK3735Wrapper
 
     # GSM/GPRS/EDGE 850/900/1800/1900 MHz
     # HSDPA/UMTS 900/2100 MHz
@@ -50,6 +88,9 @@ class HuaweiE3735(HuaweiWCDMADevicePlugin):
     version = "0.1"
     author = u"Andrew Bird"
     custom = HuaweiE3735Customizer()
+
+    # After disable / enable we must re-authenticate the SIM
+    auth_persists_over_disable = False
 
     __remote_name__ = "E3735"
 
