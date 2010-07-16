5421e41253f9de0c61544b38b5ea27e1a75371bf
Andrew Bird <ajb@spheresystems.co.uk>
2010-07-16 14:12:52 +0000

    Core - bump version in consts and packaging

--- wader/common/consts.py
+++ wader/common/consts.py
@@ -26,7 +26,7 @@ from dbus import UInt32
 # app name
 APP_NAME = 'Wader'
 APP_SLUG_NAME = 'wader-core'
-APP_VERSION = '0.5.3'
+APP_VERSION = '0.5.4'
 
 # DBus stuff
 WADER_SERVICE = 'org.freedesktop.ModemManager'
