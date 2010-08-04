95f071d5efbd4324fe05b0c1e424ff878f8e2b3a
Pablo Martí Gamboa <pmarti@warp.es>
2010-08-04 06:53:51 +0000

    with is not a keyword until 2.6

--- wader/common/provider.py
+++ wader/common/provider.py
@@ -16,6 +16,7 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 """Data providers"""
+from __future__ import with_statement
 
 import datetime
 import sqlite3
