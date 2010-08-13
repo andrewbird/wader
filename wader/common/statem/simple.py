eee7a21a40f31906e71657ae0e077b91d13a4059
Pablo Martí Gamboa <pmarti@warp.es>
2010-08-13 12:00:55 +0000

    do not hardcode conn_id in simple_statem

--- wader/common/statem/simple.py
+++ wader/common/statem/simple.py
@@ -183,7 +183,7 @@ class SimpleStateMachine(Modal):
             log.msg("Simple SM: connect exited")
 
         def do_next(self):
-            number = "*99***%d#" % self.sconn.state_dict.get('conn_id', 1)
+            number = "*99***%d#" % self.sconn.state_dict.get('conn_id')
             d = self.sconn.connect_to_internet(number)
             d.addCallback(lambda _: self.transition_to('done'))
 
