c536de94f9c0d26f27f7e27b48b4b93775a13b88
Andrew Bird <ajb@spheresystems.co.uk>
2011-08-06 14:32:43 +0000

    Core - Huawei K4510 - Add support

---
+++ plugins/devices/huawei_k4510.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2006-2011  Vodafone España, S.A.
+# Author:  Andrew Bird
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+from wader.common import consts
+from wader.common.hardware.base import build_band_dict
+from wader.common.hardware.huawei import (HuaweiWCDMADevicePlugin,
+                                          HuaweiWCDMACustomizer,
+                                          HUAWEI_BAND_DICT)
+
+
+class HuaweiK4510Customizer(HuaweiWCDMACustomizer):
+    """
+    :class:`~wader.common.hardware.huawei.HuaweiWCDMACustomizer` for the K4510
+    """
+    # GSM/GPRS/EDGE 850/900/1800/1900 MHz
+    # HSDPA/UMTS 900/2100 MHz
+
+    band_dict = build_band_dict(
+                  HUAWEI_BAND_DICT,
+                  [consts.MM_NETWORK_BAND_ANY,
+
+                   consts.MM_NETWORK_BAND_G850,
+                   consts.MM_NETWORK_BAND_EGSM,
+                   consts.MM_NETWORK_BAND_DCS,
+                   consts.MM_NETWORK_BAND_PCS,
+
+#                   consts.MM_NETWORK_BAND_U900,
+                   consts.MM_NETWORK_BAND_U2100])
+
+
+class HuaweiK4510(HuaweiWCDMADevicePlugin):
+    """:class:`~wader.common.plugin.DevicePlugin` for Huawei's K4510"""
+    name = "Huawei K4510"
+    version = "0.1"
+    author = u"Andrew Bird"
+    custom = HuaweiK4510Customizer()
+
+    __remote_name__ = "K4510"
+
+    __properties__ = {
+        'ID_VENDOR_ID': [0x12d1],
+        'ID_MODEL_ID': [0x14cb],
+    }
+
+
+huaweik4510 = HuaweiK4510()
