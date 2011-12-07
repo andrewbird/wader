84e5f37f48a96fbc671667b49d1df7989a8a6090
Andrew Bird <ajb@spheresystems.co.uk>
2011-12-07 19:44:43 +0000

    Use a common list to control backend imports

--- wader/common/backends/__init__.py
+++ wader/common/backends/__init__.py
@@ -1,6 +1,7 @@
 from wader.common.backends.nm import nm_backend
 from wader.common.backends.plain import plain_backend
 
+BACKEND_LIST = ['nm_backend', 'plain_backend']
 
 __backend = None
 
@@ -10,7 +11,8 @@ def get_backend():
     if __backend is not None:
         return __backend
 
-    for backend in [nm_backend, plain_backend]:
+    for name in BACKEND_LIST:
+        backend = globals()[name]
         if backend.should_be_used():
             __backend = backend
             return __backend
